d031e22a822fc6f105e9519c6cee6adb06bd34a3
Wallace Queiros <wallacequeiros@hotmail.com>
2024-04-26 16:43:47 -0300

    novo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //classe String que representa texto na aplicação
+
+        String meuNome = "Wallace Queiros";
+        
+        double salaraioMininmo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; //Imutavel
     }
 }
