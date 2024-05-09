aa052e26f2d35194b4f99b146b08bd1102cd9b79
Wallace Queiros <wallacequeiros@hotmail.com>
2024-05-09 15:18:50 -0300

    Exemplo Break e Continue

---
+++ controle-de-fluxo/src/ExemploContinue.java
@@ -0,0 +1,13 @@
+public class ExemploContinue {
+
+
+        public static void main(String[] args) {
+            
+            for(int numero = 1; numero <= 5; numero ++){
+                if(numero == 3)
+                    continue;
+    
+                System.out.println(numero);
+         }
+     }
+}
